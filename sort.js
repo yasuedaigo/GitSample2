c29420ee4fec7cad93d501193741bcb2349f9804
yasuedaigo <yasuedaigo77@gmail.com>
2022-05-08 23:35:17 +0900

    競合を解決

--- sort.js
+++ sort.js
@@ -1,10 +1,6 @@
 var sortNumber = function (number) {
    number.sort(function (a, b) {
-<<<<<<< HEAD
-       if (a == b) {
-=======
        if (a === b) {
->>>>>>> 5a4e86a27f2ecf53e964edf3c482f657fb5cc05e
            return 0;
        }
        return a < b ? -1 : 1;
